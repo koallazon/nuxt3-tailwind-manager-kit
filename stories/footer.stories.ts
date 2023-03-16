a483419ab647d3778582c5eadbf56216b7f34d28
ltg <ltg@bbangyanetworks.com>
2023-03-16 22:47:15 +0900

    storybook 셋팅

---
+++ stories/footer.stories.ts
@@ -0,0 +1,48 @@
+import footer from '../components/app/footer.vue'
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/vue/writing-stories/introduction
+export default {
+  title: 'Example/footer',
+  component: footer,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+    onClick: {},
+    size: {
+      control: {
+        type: 'select',
+      },
+      options: ['small', 'medium', 'large'],
+    },
+  },
+}
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/vue/writing-stories/args
+export const Primary = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+}
+
+export const Secondary = {
+  args: {
+    label: 'Button',
+  },
+}
+
+export const Large = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  },
+}
+
+export const Small = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
+}
